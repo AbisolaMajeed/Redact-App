aa2c7cee75a27eefe854f36513ef4d6136cd9cc0
Abisola Majeed <abisola.majeed@ss-limited.com>
2023-11-08 17:22:07 +0100

    allow user specify redact text

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ document.getElementById("redactBtn").addEventListener("click", function() {
 
     let text = document.getElementById("originalText").value;
     let wordsToRedact = document.getElementById("wordsToRedact").value.split(' ');
+    let redactText =  document.getElementById("redactText").value;
 
-    let redactedText = customRedact(text, wordsToRedact);
+    let redactedText = customRedact(text, wordsToRedact, redactText);
     document.getElementById("textOutputField").innerHTML = redactedText;
 });
 
-function customRedact(text, wordsToRedact) {
+function customRedact(text, wordsToRedact, redactText) {
 
     const words = text.split(' ');
 
     for (let i = 0; i < words.length; i++) {
         for (const word of wordsToRedact) {
             if (words[i].toLowerCase() === word.toLowerCase()) {
-                words[i] = '***';
+                words[i] = redactText;
             }
         }
     }
